6e2861fad1675f074c0cba0830e223ba0b235e8c
CosimoA <cosimo.archip@gmail.com>
2023-12-13 20:05:25 +0100

    FEAT: add Keyup.enter in input text

--- js/script.js
+++ js/script.js
@@ -12,20 +12,10 @@ Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 
-const todos: [
-    {
-        text: 'Fare i compiti',
-        done: false
-    },
-    {
-        text: 'Fare la spesa',
-        done: true
-    },
-    {
-        text: 'Fare il bucato',
-        done: false
-    }
-]
+Bonus:
+1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+
 */
 
 const { createApp } = Vue;
